5697f9abd4167b2076c104263f279c151fb63453
farewall <67765544@qq.com>
2019-06-12 15:19:25 +0800

    加入预览功能，方便查看录制结果.

---
+++ emedia/src/main/java/com/xjl/emedia/fragment/PreviewFragment.java
@@ -0,0 +1,95 @@
+package com.xjl.emedia.fragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.bumptech.glide.Glide;
+import com.xjl.emedia.R;
+import com.xjl.emedia.activity.VideoRecordActivity;
+import com.xjl.emedia.utils.IntentUtil;
+
+import java.io.File;
+
+/**
+ * Created by 180933664 on 2019/6/12.
+ */
+
+public class PreviewFragment extends Fragment implements View.OnClickListener {
+
+    private final String TAG = PreviewFragment.class.getSimpleName();
+
+    protected ImageView playerBackground;
+    protected ImageView player;
+    protected ImageView cancelSelect;
+    protected ImageView select;
+    protected RelativeLayout playerLayout;
+    private View rootView;
+
+    private String filePath;
+
+    public static PreviewFragment getINSTANCE(String filePath) {
+        PreviewFragment previewFragment = new PreviewFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("filePath", filePath);
+        previewFragment.setArguments(bundle);
+        return previewFragment;
+    }
+
+
+    public PreviewFragment() {
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_preview, null);
+        filePath = getArguments().getString("filePath");
+        Log.e(TAG, "filePath=" + filePath);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        playerBackground = (ImageView) rootView.findViewById(R.id.player_background);
+        player = (ImageView) rootView.findViewById(R.id.player);
+        player.setOnClickListener(PreviewFragment.this);
+        cancelSelect = (ImageView) rootView.findViewById(R.id.cancel_select);
+        cancelSelect.setOnClickListener(PreviewFragment.this);
+        select = (ImageView) rootView.findViewById(R.id.select);
+        select.setOnClickListener(PreviewFragment.this);
+        playerLayout = (RelativeLayout) rootView.findViewById(R.id.player_layout);
+
+        Glide.with(this).load("file://" + filePath)
+                .skipMemoryCache(false)
+                .centerCrop().into(playerBackground);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.player) {
+            IntentUtil.openLocalVideo(getActivity(), filePath);
+        } else if (view.getId() == R.id.cancel_select) {
+            File file = new File(filePath);
+            file.delete();
+            getActivity().setResult(VideoRecordActivity.RESULT_CODE_FOR_RECORD_VIDEO_CANCEL);
+            getActivity().finish();
+        } else if (view.getId() == R.id.select) {
+            Intent intent = new Intent();
+            intent.putExtra(VideoRecordActivity.INTENT_EXTRA_VIDEO_PATH, filePath);
+            getActivity().setResult(Activity.RESULT_OK, intent);
+            getActivity().finish();
+        }
+    }
+
+}
